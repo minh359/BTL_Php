2a27beae7658a4d70780afcfda170fb7e706dbd6
minh359 <hoangminhzzz123@gmail.com>
2021-10-11 15:59:12 +0700

    add 1st Code

---
+++ BTL/mvc/controllers/Home.php
@@ -0,0 +1,10 @@
+<?php
+    class Home{
+        public function Index(){
+            echo "đây là index";
+        }
+        public function Show($name){
+            echo "tên: ".$name;
+        }
+    }
+?>
